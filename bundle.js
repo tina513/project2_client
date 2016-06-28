c81ffe39db32f9b6d842e2d436229438153455d0
tina513 <dlnaz.ai@hotmail.com>
2016-06-28 00:03:05 -0400

    bundle

--- bundle.js
+++ bundle.js
@@ -216,7 +216,7 @@ webpackJsonp([0],[
 
 	var app = {
 	  host: 'https://tripcare.herokuapp.com',
-	  weather: 'http://api.openweathermap.org/data/2.5/weather?q='
+	  weather: 'https://api.openweathermap.org/data/2.5/weather?q='
 	};
 
 	module.exports = app;
