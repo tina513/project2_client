be507797645a35137229cbcd1a36c4c5f6e37817
tina513 <dlnaz.ai@hotmail.com>
2016-06-28 09:34:54 -0400

    for weather app

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@
 
 const app = {
   host: 'https://tripcare.herokuapp.com',
-  weather: 'http://api.wunderground.com/api/0fb1e549d95e7b8a/conditions/q/CA/San_Francisco.json'
+  weather: 'https://api.wunderground.com/api/0fb1e549d95e7b8a/conditions/q/CA/San_Francisco.json'
 };
 
 module.exports = app;
