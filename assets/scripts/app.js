52bcfe017255a53d4eddac9e469387924e751f0d
tina513 <dlnaz.ai@hotmail.com>
2016-07-08 19:09:43 -0400

    change app url

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@
 
 const app = {
   host: 'https://tripcare.herokuapp.com',
+  // host: 'http://localhost:3000',
   weather: "https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid in (select woeid from geo.places(1) where text='"
 };
 
